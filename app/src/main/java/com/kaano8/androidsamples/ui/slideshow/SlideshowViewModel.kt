76e0ee94398343fa83b9d34ef39c45a95aeb9711
rahulkanojia <kaanorahul8@gmail.com>
2020-06-14 16:17:03 +0530

    project init

---
+++ app/src/main/java/com/kaano8/androidsamples/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.kaano8.androidsamples.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
