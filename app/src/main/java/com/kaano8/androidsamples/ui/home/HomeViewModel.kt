d8f7bd7eca483e7ddbd3217347ac2a33ae9aec4c
rahulkanojia <kaanorahul8@gmail.com>
2020-06-21 20:12:17 +0530

    removed insert code from home screen

--- app/src/main/java/com/kaano8/androidsamples/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/kaano8/androidsamples/ui/home/HomeViewModel.kt
@@ -1,18 +1,11 @@
 package com.kaano8.androidsamples.ui.home
 
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
 import com.kaano8.androidsamples.database.Note
-import com.kaano8.androidsamples.database.NoteDatabaseDao
 import com.kaano8.androidsamples.repository.NoteRepository
-import kotlinx.coroutines.*
 
 class HomeViewModel(private val noteRepository: NoteRepository) : ViewModel() {
 
     val notes: LiveData<List<Note>> = noteRepository.notes
-
-    fun insertNote(note: Note) {
-        viewModelScope.launch(Dispatchers.IO) {
-            noteRepository.insert(note)
-        }
-    }
 }
