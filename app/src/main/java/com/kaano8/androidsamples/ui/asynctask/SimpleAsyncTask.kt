8629e75d8ea859de1fffa890d5dbfdd9e7ef1806
rahulkanojia <kaanorahul8@gmail.com>
2020-08-16 18:30:08 +0530

    added on progress update

--- app/src/main/java/com/kaano8/androidsamples/ui/asynctask/SimpleAsyncTask.kt
+++ app/src/main/java/com/kaano8/androidsamples/ui/asynctask/SimpleAsyncTask.kt
@@ -3,20 +3,22 @@ package com.kaano8.androidsamples.ui.asynctask
 import android.os.AsyncTask
 import android.widget.TextView
 
-class SimpleAsyncTask(private val textView: TextView?): AsyncTask<Unit, Int, String>() {
+class SimpleAsyncTask(private val textView: TextView?) : AsyncTask<Unit, Int, String>() {
     override fun doInBackground(vararg params: Unit?): String {
         // Construct a random number
         val randomNumber = (1..10).shuffled().first()
 
-        val timer = randomNumber * 1000L
-
-        try {
-            Thread.sleep(timer)
-        } catch (exception: Exception) {
-            exception.printStackTrace()
+        for (i in randomNumber downTo 1) {
+            // check for isCancelled() here
+            publishProgress(i)
+            Thread.sleep(1000)
         }
-
         return "Your app slept for $randomNumber secs.."
+    }
+
+    override fun onProgressUpdate(vararg values: Int?) {
+        super.onProgressUpdate(*values)
+        textView?.text = "Waking app in ${values[0]} secs."
     }
 
     override fun onPostExecute(result: String?) {
