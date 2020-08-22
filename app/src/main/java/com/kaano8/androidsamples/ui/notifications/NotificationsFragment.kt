465a0351cfcd715273f2426a95db3bd2da01619e
rahulkanojia <kaanorahul8@gmail.com>
2020-08-23 00:49:19 +0530

    updated notification using PI

--- app/src/main/java/com/kaano8/androidsamples/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/kaano8/androidsamples/ui/notifications/NotificationsFragment.kt
@@ -3,8 +3,11 @@ package com.kaano8.androidsamples.ui.notifications
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Context.NOTIFICATION_SERVICE
 import android.content.Intent
+import android.content.IntentFilter
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Build
@@ -14,6 +17,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.NotificationCompat
 import androidx.fragment.app.Fragment
+import com.kaano8.androidsamples.BuildConfig
 import com.kaano8.androidsamples.MainActivity
 import com.kaano8.androidsamples.R
 import com.kaano8.androidsamples.util.extensions.showToast
@@ -23,11 +27,13 @@ class NotificationsFragment : Fragment() {
 
     private lateinit var notificationManager: NotificationManager
     private lateinit var notificationBuilder: NotificationCompat.Builder
+    private val notificationReceiver = NotificationReceiver()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        activity?.registerReceiver(notificationReceiver, IntentFilter(ACTION_UPDATE_NOTIFICATION))
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_notifications, container, false)
     }
@@ -37,22 +43,17 @@ class NotificationsFragment : Fragment() {
         initNotificationManger()
         setupNotificationChannel()
         setNotificationButtonState(true, false, false)
-        notify?.setOnClickListener {
-            sendNotification()
-        }
+        // Click listener for notify button
+        notify?.setOnClickListener { sendNotification() }
+        // Click listener for update button
+        update?.setOnClickListener { updateNotification() }
+        // Click listener for cancel button
+        cancel?.setOnClickListener { cancelButton() }
+    }
 
-        cancel?.setOnClickListener {
-            if (::notificationManager.isInitialized) {
-                notificationManager.cancel(NOTIFICATION_ID)
-                setNotificationButtonState(true, false, false)
-            }
-            else
-                activity?.showToast("Notification manager not init yet!")
-        }
-
-        update?.setOnClickListener {
-            updateNotification()
-        }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        activity?.unregisterReceiver(notificationReceiver)
     }
 
     private fun initNotificationManger() {
@@ -76,7 +77,9 @@ class NotificationsFragment : Fragment() {
     }
 
     private fun sendNotification() {
+
         activity?.let {
+            val updatePendingIntent = PendingIntent.getBroadcast(activity, NOTIFICATION_ID, Intent(ACTION_UPDATE_NOTIFICATION), PendingIntent.FLAG_ONE_SHOT)
             notificationBuilder = NotificationCompat.Builder(it, PRIMARY_CHANNEL_ID)
                 .setContentTitle("You've been notified!")
                 .setContentText("This is your notification text.")
@@ -91,12 +94,13 @@ class NotificationsFragment : Fragment() {
                 )
                 .setAutoCancel(true)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .addAction(R.drawable.ic_update_action, "Update Notification", updatePendingIntent)
             notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
             setNotificationButtonState(false, true, true)
         }
     }
 
-    private fun updateNotification() {
+    fun updateNotification() {
         val androidImage = BitmapFactory.decodeResource(resources, R.drawable.mascot_1)
         if (::notificationBuilder.isInitialized) {
             notificationBuilder.setStyle(
@@ -111,6 +115,15 @@ class NotificationsFragment : Fragment() {
         }
     }
 
+    private fun cancelButton() {
+        if (::notificationManager.isInitialized) {
+            notificationManager.cancel(NOTIFICATION_ID)
+            setNotificationButtonState(true, false, false)
+        }
+        else
+            activity?.showToast("Notification manager not init yet!")
+    }
+
     private fun setNotificationButtonState(isNotifyEnabled: Boolean, isUpdateEnabled: Boolean, isCancelEnabled: Boolean) {
         notify?.isEnabled = isNotifyEnabled
         update?.isEnabled = isUpdateEnabled
@@ -121,5 +134,12 @@ class NotificationsFragment : Fragment() {
         private const val PRIMARY_CHANNEL_ID = "primary_notification_channel"
         private const val MASCOT_NOTIFICATION_CHANNEL_NAME = "Mascot Notification Channel"
         private const val NOTIFICATION_ID = 0
+        private const val ACTION_UPDATE_NOTIFICATION = BuildConfig.APPLICATION_ID + "ACTION_UPDATE_NOTIFICATION"
+    }
+
+    inner class NotificationReceiver: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            updateNotification()
+        }
     }
 }
