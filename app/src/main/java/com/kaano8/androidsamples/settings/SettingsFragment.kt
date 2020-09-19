cfb60598ddddc8657ebf837f01c0163787676232
rahulkanojia <kaanorahul8@gmail.com>
2020-09-19 15:39:01 +0530

    implemented dark mode

--- app/src/main/java/com/kaano8/androidsamples/settings/SettingsFragment.kt
+++ app/src/main/java/com/kaano8/androidsamples/settings/SettingsFragment.kt
@@ -1,12 +1,26 @@
 package com.kaano8.androidsamples.settings
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
 import com.kaano8.androidsamples.R
 
 class SettingsFragment: PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_prefs, rootKey)
+        val darkModeSwitch = findPreference<SwitchPreferenceCompat>("darkMode")
+        darkModeSwitch?.onPreferenceChangeListener =
+            Preference.OnPreferenceChangeListener { preference, newValue ->
+                if (newValue == true)
+                    AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+                else
+                    AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+                true
+            }
     }
 }
