b593dbdffe82e84b46357739c35b67b668411c72
kaano-8 <kaanorahul8@gmail.com>
2020-10-01 08:24:20 +0530

    added room test

---
+++ app/src/androidTest/java/com/kaano8/androidsamples/StudentEntityReadWriteTest.kt
@@ -0,0 +1,61 @@
+package com.kaano8.androidsamples
+
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.kaano8.androidsamples.database.AppDatabase
+import com.kaano8.androidsamples.database.student.Student
+import com.kaano8.androidsamples.database.student.StudentDao
+import com.kaano8.androidsamples.extensions.getOrAwaitValue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.After
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+class StudentEntityReadWriteTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var studentDao: StudentDao
+    private lateinit var database: AppDatabase
+
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        database = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).allowMainThreadQueries().build()
+        studentDao = database.studentDao
+    }
+
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        database.close()
+    }
+
+
+    @Test
+    fun insertStudent() = runBlockingTest {
+        val student = Student(firstName = "Rahul", lastName = "Kanojia", currentClass = "12")
+        studentDao.insertStudent(student)
+
+        val firstStudent = studentDao.getAllStudents().getOrAwaitValue().first()
+        assertThat(firstStudent.firstName, equalTo("Rahul"))
+    }
+}
